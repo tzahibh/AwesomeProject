abf05070e054abf627abfc04a905a00c820f08e4
tzahibh <tzahibh@matrix.co.il>
2024-05-21 11:37:24 +0300

    added react-query

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import {
   SafeAreaView,
   Text,
 } from 'react-native';
-
+import {
+  QueryClient,
+} from '@tanstack/react-query'
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { Counter } from './features/counter/counter';
 
+const queryClient = new QueryClient()
 
 function App(): React.JSX.Element {
 
